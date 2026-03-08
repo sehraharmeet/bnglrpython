e209991cc1a35e64dc10fb86823f3475129ebd7c
bhargavitummala1253 <bhanutummala6357@gmail.com>
2026-03-09 00:31:43 +0530

    Add files via upload

---
+++ Project from PAX/Bhargavi_CPP/IncomeExpenseTracker/IncomeExpenseTracker/src/main.cpp
@@ -0,0 +1,34 @@
+﻿#include "Tracker/Tracker.hpp"
+#include <iostream>
+
+int main() {
+    Tracker tracker;
+    int choice = 0;
+
+    do {
+        std::cout << "\n=== Income & Expense Tracker ===\n"
+            << "1. Add Transaction\n"
+            << "2. Show All\n"
+            << "3. Show Summary\n"
+            << "4. Show by Category\n"
+            << "5. Show Summary Threaded\n"
+            << "6. Show with Date\n"
+            << "0. Exit\n"
+            << "Enter choice: ";
+        std::cin >> choice;
+        std::cin.ignore();
+
+        switch (choice) {
+        case 1: tracker.addTransaction(); break;
+        case 2: tracker.showAll(); break;
+        case 3: tracker.showSummary(); break;
+        case 4: tracker.showByCategory(); break;
+        case 5: tracker.showSummaryThreaded(); break;
+        case 6: tracker.showWithDate(); break;
+        case 0: std::cout << "Goodbye!\n"; break;
+        default: std::cout << "Invalid choice\n";
+        }
+    } while (choice != 0);
+
+    return 0;
+}
